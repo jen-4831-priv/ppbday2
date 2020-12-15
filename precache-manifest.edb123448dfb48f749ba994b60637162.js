0baf912dfe703bb2033fab357e27c258326f96dd
nzhe4831 <jennnai.rl4831@gmail.com>
2020-12-15 21:35:11 +1100

    Updates

---
+++ precache-manifest.edb123448dfb48f749ba994b60637162.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "808518e3af7115e7346b241448dd506a",
+    "url": "/ppbday2/index.html"
+  },
+  {
+    "revision": "30b22ad457c6d3a121c9",
+    "url": "/ppbday2/static/css/main.15ee3c3b.chunk.css"
+  },
+  {
+    "revision": "2a87c185e49fce136cdb",
+    "url": "/ppbday2/static/js/2.a66291f0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ppbday2/static/js/2.a66291f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30b22ad457c6d3a121c9",
+    "url": "/ppbday2/static/js/main.9c888e4a.chunk.js"
+  },
+  {
+    "revision": "fde53914c50e4ebd22b8",
+    "url": "/ppbday2/static/js/runtime-main.30ac3b5f.js"
+  }
+]);
